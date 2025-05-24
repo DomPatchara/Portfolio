abbc28e76a4047e62f05c072be8cdb6a09c53ac4
DomPatchara <domdoo2015@gmail.com>
2025-05-24 23:05:18 +0700

    fixed

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,6 +1,6 @@
 import { workData } from "../../assets/assets";
 import useSectionInview from "../hook/useInView";
-import Card from "./ui/Card";
+import Card from './ui/card'
 import { useRef } from "react";
 import { useScroll, motion } from "framer-motion";
 
